9fa28667ece4f5bac5f0cbd865e47bc3efdd6903
regina77512 <turaevaregina@gmail.com>
2025-09-15 08:16:31 +0400

    Эксперименты с функциями

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -2,5 +2,15 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("Hello, world");
+
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 - 2);
+    System.out.println(2 / 2);
+    System.out.println((2 + 2) * 2);
+
+    System.out.println("Hello, " + "world!");
+    System.out.println("2 + 2 = " + (2 + 2));
+
   }
 }
