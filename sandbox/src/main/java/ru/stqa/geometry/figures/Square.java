ccc1deb4a0a14f9437bbdf7a7a5f930866302d7c
regina77512 <turaevaregina@gmail.com>
2025-09-16 09:16:30 +0400

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry.figures;
+
+public class Square {
+
+  public static void printSquareArea(double side) {
+    String text = String.format("The area of a square with a side %f = %f", side, squareArea(side));
+    System.out.println(text);
+  }
+
+  private static double squareArea(double a) {
+    return a * a;
+  }
+}
+
