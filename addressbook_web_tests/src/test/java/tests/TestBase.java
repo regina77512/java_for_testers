368232b93975fe6f609dfe78b0d70714d27d5e04
regina77512 <turaevaregina@gmail.com>
2025-10-06 23:45:08 +0400

    Выполнено ДЗ №8

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -11,8 +11,9 @@ public class TestBase {
   public void setUp() {
     if (app == null) {
       app = new ApplicationManager();
+      app.init(System.getProperty("browser","firefox"));
     }
-    app.init();
+
   }
 
 }
