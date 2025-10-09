23e3d700ffa3516fded1faa07d6bd89ca6d952e2
regina77512 <turaevaregina@gmail.com>
2025-10-10 00:27:23 +0400

    Выполнено ДЗ №6

---
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  void canModifyGroup(){
+    if (!app.groups().isGroupPresent()){          //если нет ни одной группы, создаем новую
+      app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+    }
+    app.groups().modifyGroup(new GroupData().withName("modified name"));      // вызов метода, который модифицирует группу
+    //метод modifyGroup, у которого в качестве параметра передается объект типа GroupData, который содержит новый набор свойств
+  }
+
+}
