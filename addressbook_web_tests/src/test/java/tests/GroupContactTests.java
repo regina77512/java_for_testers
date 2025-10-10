9fefdfb53ccfbd4407e23e1ada0a0b7eac853ec2
regina77512 <turaevaregina@gmail.com>
2025-10-10 19:30:56 +0400

    Создание groupContactTests

---
+++ addressbook_web_tests/src/test/java/tests/GroupContactTests.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import tests.TestBase;
+
+public class GroupContactTests{
+private WebDriver driver;
+
+  @BeforeEach
+  public void setup(){
+    driver = new FirefoxDriver();
+    driver.get("http://localhost/addressbook/");
+    driver.manage().window().setSize(new Dimension(966, 677));
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+  }
+
+
+
+  @AfterEach
+  public void tearDown(){
+    driver.quit();
+  }
+
+
+  @Test
+  public void canCreateContact() {
+
+    driver.findElement(By.linkText("add new")).click();
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).sendKeys("Иванов");
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).sendKeys("Иван");
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).sendKeys("Иванович");
+    driver.findElement(By.name("mobile")).click();
+    driver.findElement(By.name("mobile")).sendKeys("667788");
+    driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+    driver.findElement(By.linkText("home page")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+}
