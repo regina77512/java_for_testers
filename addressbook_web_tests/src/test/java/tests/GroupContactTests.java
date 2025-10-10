d0ce5a3d14bf9e81494d615d1ede5196ea22f737
regina77512 <turaevaregina@gmail.com>
2025-10-10 21:22:35 +0400

    Перенос вспомогательных методов из GroupContactTests в TestBase

--- addressbook_web_tests/src/test/java/tests/GroupContactTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupContactTests.java
@@ -1,56 +1,12 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import tests.TestBase;
 
-public class GroupContactTests {
-
-  private static WebDriver driver;
-
-  @BeforeEach
-  public void setup() {
-    if (driver == null) {
-      driver = new FirefoxDriver();
-      Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-      driver.get("http://localhost/addressbook/");
-      driver.manage().window().setSize(new Dimension(966, 677));
-      driver.findElement(By.name("user")).sendKeys("admin");
-      driver.findElement(By.name("pass")).sendKeys("secret");
-      driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-    }
-  }
+public class GroupContactTests extends TestBase{
 
   @Test
   public void canCreateContact() {
-    if (!isElementPresent(By.xpath("//h1[contains(text(),'Edit / add address book entry')]"))) {
-      driver.findElement(By.linkText("add new")).click();
-    }
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).sendKeys("Иванов");
-    driver.findElement(By.name("middlename")).click();
-    driver.findElement(By.name("middlename")).sendKeys("Иван");
-    driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).sendKeys("Иванович");
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).sendKeys("667788");
-    driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
-    driver.findElement(By.linkText("home page")).click();
-    driver.findElement(By.linkText("Logout")).click();
-  }
-
-  private boolean isElementPresent(By locator) {
-    try {
-      driver.findElement(locator);
-      return true;
-    } catch (NoSuchElementException exception) {
-      return false;
-    }
+    openContactPage();
+    createContact("Петров", "Петр", "Петрович");
   }
 }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -2,10 +2,16 @@ package tests;
 
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestBase {
 
   protected static ApplicationManager app;
+
 
   @BeforeEach
   public void setUp() {
@@ -16,4 +22,50 @@ public class TestBase {
 
   }
 
+
+
+  // нужно будет удалить - driver, setup,isElementPresent
+  protected static WebDriver driver;
+
+  @BeforeEach
+  public void setup() {
+    if (driver == null) {
+      driver = new FirefoxDriver();
+      Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+      driver.get("http://localhost/addressbook/");
+      driver.manage().window().setSize(new Dimension(966, 677));
+      driver.findElement(By.name("user")).sendKeys("admin");
+      driver.findElement(By.name("pass")).sendKeys("secret");
+      driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+    }
+  }
+
+  protected boolean isElementPresent(By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException exception) {
+      return false;
+    }
+  }
+  //
+
+  protected void createContact(String firstName, String middleName, String lastName) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).sendKeys(firstName);
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).sendKeys(middleName);
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).sendKeys(lastName);
+    driver.findElement(By.name("mobile")).click();
+    driver.findElement(By.name("mobile")).sendKeys("667788");
+    driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+    driver.findElement(By.linkText("home page")).click();
+  }
+
+  protected void openContactPage() {
+    if (!isElementPresent(By.xpath("//h1[contains(text(),'Edit / add address book entry')]"))) {
+      driver.findElement(By.linkText("add new")).click();
+    }
+  }
 }
