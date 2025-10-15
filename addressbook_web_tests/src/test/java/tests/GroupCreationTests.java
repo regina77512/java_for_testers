15fc7005061249185a7a728876f9fbe11ca4b87c
regina77512 <turaevaregina@gmail.com>
2025-10-15 23:33:34 +0400

    Лекция 4.5. Параметризованные тесты для создания групп

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,40 +1,50 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.List;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void canCreateGroup() {
+  public static List<GroupData> groupProvider() {
+    var result = new ArrayList<GroupData>();
+    for (var name : List.of("", "group name")) {
+      for (var header : List.of("", "group header")) {
+        for (var footer : List.of("", "group footer")) {
+          result.add(new GroupData(name, header, footer));
+        }
+      }
+    }
+    for (int i = 0; i < 5; i++) {
+      result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+    }
+    return result;
+  }
+
+  public static List<GroupData> negativeGroupProvider() {
+    var result = new ArrayList<GroupData>(List.of(
+        new GroupData("group name'", "", "")));
+    return result;
+  }
+
+  @ParameterizedTest
+  @MethodSource("groupProvider")
+  public void canCreateMultipleGroups(GroupData group) {
     int groupCount = app.groups().getCount();
-    app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+    app.groups().createGroup(group);
     int newGroupCount = app.groups().getCount();
     Assertions.assertEquals(groupCount + 1, newGroupCount);
   }
 
-  @Test
-  public void canCreateGroupWithEmptyName() {
-    app.groups().createGroup(new GroupData());
-  }
-
-  @Test
-  public void canCreateGroupWithNameOnly() {
-    app.groups().createGroup(new GroupData().withName("some name"));
-  }
-
-  @Test
-  public void canCreateMultipleGroups() {
-    int n = 5;
+  @ParameterizedTest
+  @MethodSource("negativeGroupProvider")
+  public void canNotCreateMultipleGroups(GroupData group) {
     int groupCount = app.groups().getCount();
-
-    for (int i = 0; i < n; i++){
-      app.groups().createGroup(new GroupData(randomString(i * 10), "group header", "group footer"));
-    }
-
-
+    app.groups().createGroup(group);
     int newGroupCount = app.groups().getCount();
-    Assertions.assertEquals(groupCount + n, newGroupCount);
+    Assertions.assertEquals(groupCount, newGroupCount);
   }
 }
