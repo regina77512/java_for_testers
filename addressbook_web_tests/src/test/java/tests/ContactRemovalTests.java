fe173183cfd25e653ceb1ae39b081833ea93f5fa
regina77512 <turaevaregina@gmail.com>
2025-10-18 00:10:34 +0400

    Подсчет кол-ва эл-тов в тесте для удаления контактов

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,15 +1,19 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactRemovalTests extends TestBase{
 
   @Test
   void canRemoveContact(){
-    if (!app.contacts().isContactPresent()){
+    if (app.contacts().getCount() == 0){
       app.contacts().createContact(new ContactData("Сергей", "Колодежнов", "ул.Тухачевского 7", "112122","еук@mail.ru"));
     }
+    int contactCount = app.contacts().getCount();
     app.contacts().removeContact();
+    int newContactCount = app.contacts().getCount();
+    Assertions.assertEquals(contactCount - 1, newContactCount);
   }
 }
