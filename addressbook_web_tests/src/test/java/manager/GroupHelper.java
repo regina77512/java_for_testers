96573c361e45ca3af2f42dab2e09b847fb731183
regina77512 <turaevaregina@gmail.com>
2025-11-05 12:54:32 +0400

    Лекция 7.1. Функциональный стиль программирования

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import model.GroupData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class GroupHelper extends HelperBase {
 
@@ -90,10 +91,9 @@ public class GroupHelper extends HelperBase {
   }
 
   private void selectAllGroups() {
-    var checkboxes = manager.driver.findElements(By.name("selected[]"));
-    for (var checkbox : checkboxes) {
-      checkbox.click();
-    }
+    manager.driver
+        .findElements(By.name("selected[]"))
+        .forEach(WebElement::click);
   }
 
   public List<GroupData> getList() {
--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry;
 
+import java.util.List;
+import java.util.function.Consumer;
 import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
 import ru.stqa.geometry.figures.Triangle;
@@ -7,19 +9,23 @@ import ru.stqa.geometry.figures.Triangle;
 public class Geometry {
 
   public static void main(String[] args) {
-    Square.printSquareArea(new Square(7));
-    Square.printSquareArea(new Square(5));
-    Square.printSquareArea(new Square(3));
-    
-    Rectangle.printRectangleArea(3.0, 5.0);
-    Rectangle.printRectangleArea(7.0, 9.0);
+    var squares = List.of(new Square(7), new Square(5), new Square(3));
+//    for (Square square : squares) {
+//      Square.printSquareArea(square);
+//    }
 
-    Triangle.printTriangleArea(new Triangle(6,8,10));
-    Triangle.printTriangleArea(new Triangle(9,10,17));
+    squares.forEach(Square::printSquareArea);//forEach - метод у списка, в который можно передать функцию и эта ф-ция
+    // будет применена ко всем эл-там списка, бывает 3 типа ф-ции:
+    // - consumer - принимает на вход параметр, но ничего не возвращает
+    // - producer - не принимает ничего на вход, но что-то возвращает
+    // - настоящая ф-ция - что-то принимает на вход и что-то возвращает
 
-    Triangle.printTrianglePerimeter(new Triangle(3,5,7.0));
-
-
+//    Rectangle.printRectangleArea(3.0, 5.0);
+//    Rectangle.printRectangleArea(7.0, 9.0);
+//
+//    Triangle.printTriangleArea(new Triangle(6,8,10));
+//    Triangle.printTriangleArea(new Triangle(9,10,17));
+//
+//    Triangle.printTrianglePerimeter(new Triangle(3,5,7.0));
   }
-
 }
