b46c7391328c0ef81a3c357eb8a20bc11a9325ff
regina77512 <turaevaregina@gmail.com>
2025-11-10 07:06:25 +0400

    Выполнено ДЗ №17

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,45 @@
+package ru.stqa.mantis.manager;
+
+import java.util.Properties;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ApplicationManager {
+
+  private WebDriver driver;
+  private String string;
+  private Properties properties;
+  private SessionHelper sessionHelper;
+
+
+  public void init(String browser, Properties properties) {
+    this.string = browser;
+    this.properties = properties;
+   
+  }
+
+  public WebDriver driver() {
+    if (driver == null) {
+      if ("firefox".equals(string)){
+        driver = new FirefoxDriver();
+      }else if ("chrome".equals(string)){
+        driver = new ChromeDriver();
+      }else {
+        throw new IllegalArgumentException(String.format("Unknown browser %s", string));
+      }
+      Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+      driver.get(properties.getProperty("web.baseUrl"));
+      driver.manage().window().setSize(new Dimension(1076, 640));
+    }
+    return driver;
+  }
+
+  public SessionHelper session() {
+    if (sessionHelper == null) {
+      sessionHelper = new SessionHelper(this);
+    }
+    return sessionHelper;
+  }
+}
